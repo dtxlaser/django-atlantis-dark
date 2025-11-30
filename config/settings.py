f0e9d0e5029005d6e6ddfe6a4f77f325632981e2
dtxlaser <dtx.laser@gmail.com>
2025-11-30 22:49:50 +0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -29,11 +29,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'Super_Secr3t_9999')
 DEBUG = 'RENDER' not in os.environ
 
 # HOSTs List
-#ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
 # Add here your deployment HOSTS
-#CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://localhost:5085', 'http://127.0.0.1:8000', 'http://127.0.0.1:5085']
+# CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://localhost:5085', 'http://127.0.0.1:8000', 'http://127.0.0.1:5085']
 
 # CSRF trusted origins (for HTTPS)
 CSRF_TRUSTED_ORIGINS = [
